102e6e8be92dc0211ed946ba04334c57109fa4ed
Wang Yubo <charleskeeling65@163.com>
2025-04-07 15:10:25 +0800

    fixed: setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ setup(
     name="ManureTransport",
     version="0.1.0",
     author="wangyb",
-    author_email="your.email@example.com",
+    author_email="charleskeeling65@163.com",
     description="粪便养分运输优化和分配库",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/ManureTransport",
+    url="https://github.com/CharlesKeeling65/ManureTransport",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
